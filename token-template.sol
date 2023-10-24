f56006b48fc8e1d714b2db53831f65c4f0e43f72
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-24 08:44:49 +0300

    token-template.sol

---
+++ token-template.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+
+contract Token {
+    uint public totalSupply;
+    string public name = "excelcium";
+    string public symbol = "exc";
+    uint8 public decimals = 18;
+
+    mapping(address => uint256) balance;
+
+    function balanceOf(address _owner) external view returns(uint _balance) {
+        _balance = balance[_owner];
+    }
+
+}
