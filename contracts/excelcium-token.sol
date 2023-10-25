7aafa6e4f7227108a2735d7fb871e42cad06baf2
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-25 10:28:38 +0300

    update

--- contracts/excelcium-token.sol
+++ contracts/excelcium-token.sol
@@ -4,8 +4,10 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Excelcium is ERC20 {
-    uint constant _initial_supply = (10**8) * (10**18);
+    uint public _initial_supply = (10**8) * (10**18);
     constructor() ERC20("Excelcium", "EXC") {
         _mint(msg.sender, _initial_supply);
     }
 }
+
+
