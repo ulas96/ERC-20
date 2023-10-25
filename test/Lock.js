f931515e073c68bced17610685648dfc4c3a4283
ulas96 <y.ulas.yildiz@gmail.com>
2023-10-25 10:22:24 +0300

    update

--- test/Lock.js
+++ test/Lock.js
@@ -16,7 +16,7 @@ describe("Lock", function () {
     const lockedAmount = ONE_GWEI;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // Contracts are deployed using the first signer/account by default
+    // contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Lock = await ethers.getContractFactory("Lock");
